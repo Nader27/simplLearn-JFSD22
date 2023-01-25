4200fed7c14f05cab752d5b73a8a9f4a2d89c311
Nader Labib <nader_alaa@hotmail.com>
2023-01-25 21:05:19 +0200

    Phase 1 Logic implemented

---
+++ Phase 1/src/com/LockedMe/Main.java
@@ -0,0 +1,134 @@
+package com.LockedMe;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    private static File rootDirectory = null;
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    private static void clearConsole() {
+        try {
+            final String os = System.getProperty("os.name");
+
+            if (os.contains("Windows")) {
+                Runtime.getRuntime().exec("cls");
+            } else {
+                Runtime.getRuntime().exec("clear");
+            }
+        } catch (final Exception e) {
+            //  Handle any exceptions.
+        }
+    }
+
+    private static void clearScanner() {
+        scanner.nextLine();
+    }
+
+    private static void selectRootDirectory() {
+        do {
+            if (rootDirectory != null)
+                System.out.println("Directory path is not valid!!");
+            System.out.println("Enter The Root Directory:");
+            String directory = scanner.nextLine();
+            try {
+                rootDirectory = new File(directory);
+            } catch (Exception e) {
+                System.out.println("Error " + e.getMessage());
+            }
+        } while (!rootDirectory.isDirectory());
+    }
+
+    private static int mainMenuDisplay() {
+        int selectedOption = 0;
+        while (selectedOption < 1 || selectedOption > 6) {
+            clearConsole();
+            System.out.println("Hello Welcome to LockedMe");
+            System.out.println("Developed by: Nader Labib\n");
+            System.out.println("Please select any of the below options to proceed:\n");
+            System.out.println("(1) List files");
+            System.out.println("(2) Create a new file");
+            System.out.println("(3) Delete an existing file");
+            System.out.println("(4) Search for a file");
+            System.out.println("(5) Change Root Directory");
+            System.out.println("(6) Exit Application");
+            try {
+                selectedOption = scanner.nextInt();
+            } catch (Exception e) {
+                System.out.println("Error " + e.getMessage());
+            }
+        }
+        clearScanner();
+        return selectedOption;
+    }
+
+    public static void main(String[] args) {
+        selectRootDirectory();
+        boolean exit = false;
+        while (!exit) {
+            switch (mainMenuDisplay()) {
+                case 1:
+                    List<File> fileList = Operations.list(rootDirectory);
+                    for (int i = 0; i < fileList.size(); i++) {
+                        System.out.println("(" + (i + 1) + ") " + fileList.get(i).getName());
+                    }
+                    System.out.print("Press enter to continue.....");
+                    scanner.nextLine();
+                    break;
+                case 2:
+                    System.out.println("Enter file Name :");
+                    String newFileName = scanner.nextLine();
+                    if (Operations.add(newFileName, rootDirectory)) {
+                        System.out.println("File created successfully !!");
+                        scanner.nextLine();
+                    } else {
+                        System.out.println("File failed to be created !!");
+                        scanner.nextLine();
+                    }
+                    break;
+                case 3:
+                    System.out.println("Enter file Name :");
+                    String deleteFileName = scanner.nextLine();
+                    if (Operations.delete(deleteFileName, rootDirectory)) {
+                        System.out.println("File is deleted !!");
+                        scanner.nextLine();
+                    } else {
+                        System.out.println("File is not deleted !!");
+                        scanner.nextLine();
+                    }
+                    break;
+                case 4:
+                    System.out.println("Enter file Name :");
+                    String fileName = scanner.nextLine();
+                    if (Operations.search(fileName, rootDirectory)) {
+                        System.out.println("File is exist !!");
+                        scanner.nextLine();
+                    } else {
+                        System.out.println("File is not exist !!");
+                        scanner.nextLine();
+                    }
+                    break;
+                case 5:
+                    rootDirectory = null;
+                    selectRootDirectory();
+                    break;
+                case 6:
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Not Valid option Selected");
+                    break;
+            }
+        }
+
+        System.out.println("Thanks for using LockedMe");
+        System.out.println("Goodbye!!");
+    }
+}
--- Phase 1/src/com/LockedMe/Operations.java
+++ Phase 1/src/com/LockedMe/Operations.java
@@ -1,5 +1,48 @@
 package com.LockedMe;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class Operations {
 
+    public static List<File> list(File rootDirectory){
+        List<File> directories = Arrays.asList(rootDirectory.listFiles());
+        sort(directories);
+        return directories;
+    }
+
+    private static void sort(List<File> directory){
+        directory.sort(Comparator.comparing(File::getName));
+    }
+
+    public static boolean add(String fileName,File rootDirectory) {
+        File newfile = new File(rootDirectory.getAbsolutePath()+File.separator+fileName);
+        try {
+            return newfile.createNewFile();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static boolean search(String fileName, File rootDirectory) {
+        File newfile = new File(rootDirectory.getAbsolutePath()+File.separator+fileName);
+        try {
+            return newfile.exists();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static boolean delete(String fileName, File rootDirectory) {
+        File newfile = new File(rootDirectory.getAbsolutePath()+File.separator+fileName);
+        try {
+            return newfile.delete();
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
